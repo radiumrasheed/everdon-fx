417757ea1f2459a590cdc31564d79510508fecb8
Rasheed Rahman <ra1da35ma@gmail.com>
2018-07-05 21:08:30 +0100

    Revert "Use Self-Signed Certificate"
    This reverts commit c26d55e
    
    Signed-off-by: Rasheed Rahman <ra1da35ma@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://54.245.56.151/api',
+  API_URL: 'http://54.245.56.151/api',
   PUSHER_KEY: '95090bf81a47a6306833',
   PUSHER_CLUSTER: 'eu'
 };
